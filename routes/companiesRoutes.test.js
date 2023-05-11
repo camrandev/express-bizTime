e6469ee282e6b8d7337e609aad5727888902ce61
Ashley Lin <ashleyylin11@gmail.com>
2023-05-11 15:49:28 -0700

    add invoices routes and tests for companiesRoutes

---
+++ routes/companiesRoutes.test.js
@@ -0,0 +1,130 @@
+"use strict";
+
+const request = require("supertest");
+
+const app = require("../app");
+const db = require("../db");
+
+let testCompany;
+
+beforeEach(async function () {
+  await db.query("DELETE FROM companies");
+  let result = await db.query(`
+    INSERT INTO companies (code, name, description)
+    VALUES ('apple', 'Apple Computer', 'A fast computer')
+    RETURNING code, name, description`);
+  testCompany = result.rows[0];
+});
+
+/** GET /companies: returns list of companies like
+ * {companies: [{code, name}, ...]} */
+
+describe("GET /companies", function () {
+  test("Gets companies", async function () {
+    const resp = await request(app).get(`/companies`);
+    console.log(resp.body);
+    expect(resp.body).toEqual({
+      companies: [{ code: testCompany.code, name: testCompany.name }],
+    });
+  });
+});
+
+/** GET /companies/[code]: returns object of company with the associated code
+ * in the query string: {company: {code, name, description}}
+
+ * If company isn't found, returns NotFoundError
+*/
+
+describe("GET /companies/:code", function () {
+  test("Gets single company", async function () {
+    const resp = await request(app).get(`/companies/${testCompany.code}`);
+    expect(resp.body).toEqual({ company: testCompany });
+  });
+
+  test("404 if not found", async function () {
+    const resp = await request(app).get(`/companies/-1`);
+    expect(resp.statusCode).toEqual(404);
+  });
+});
+
+
+/** POST /companies: accepts JSON body, adds company to database, returns
+ * added object of added company
+ *
+ * Takes as input JSON: {code, name, description}
+   Returns object of company: {company: {code, name, description}}
+
+   If no input JSON, throws BadRequestError
+ */
+
+describe("POST /companies", function () {
+  test("Create new company", async function () {
+    const resp = await request(app)
+      .post(`/companies`)
+      .send({ code: "ibm", name: "IBM company", description: "Some company" });
+    expect(resp.statusCode).toEqual(201);
+    expect(resp.body).toEqual({
+      company: { code: "ibm", name: "IBM company", description: "Some company" },
+    });
+  });
+
+  test("400 if empty request body", async function () {
+    const resp = await request(app)
+      .post(`/companies`)
+      .send();
+    expect(resp.statusCode).toEqual(400);
+  });
+});
+
+
+/** PUT /companies/[code]: accepts JSON body, edits existing company in database,
+ * returns updated company object.
+ *
+ * Takes as input JSON: {name, description}
+   Returns object of company: {company: {code, name, description}}
+
+   If company is not found, throws NotFoundError. If no input JSON is given,
+   throws BadRequestError
+ */
+
+describe("PUT /companies/:code", function () {
+  test("Update single company", async function () {
+    const resp = await request(app)
+      .put(`/companies/${testCompany.code}`)
+      .send({ name: "New name", description: "New description" });
+    expect(resp.statusCode).toEqual(200);
+    expect(resp.body).toEqual({
+      company: { code: testCompany.code, name: "New name", description: "New description" },
+    });
+  });
+
+  test("404 if not found", async function () {
+    const resp = await request(app)
+      .put(`/companies/-1`)
+      .send({ name: "New name", description: "New description" });
+    expect(resp.statusCode).toEqual(404);
+  });
+
+  test("400 if empty request body", async function () {
+    const resp = await request(app)
+      .put(`/companies/${testCompany.code}`)
+      .send();
+    expect(resp.statusCode).toEqual(400);
+  });
+});
+
+
+/** DELETE /companies/[code].
+ *
+ * Deletes compan with the given code, and returns {status: "deleted"}
+ * if successful. If company not found, throws NotFoundError.
+ */
+
+describe("DELETE /companies/:code", function () {
+  test("Delete single company", async function () {
+    const resp = await request(app)
+      .delete(`/companies/${testCompany.code}`);
+    expect(resp.statusCode).toEqual(200);
+    expect(resp.body).toEqual({ status: "deleted" });
+  });
+});
--- routes/invoices.js
+++ routes/invoices.js
@@ -13,11 +13,10 @@ const {
 
 const router = new express.Router();
 
-/** GET /invocies: returns list of invocies like
+/** GET /invoices: returns list of invoices like
  * {invoices: [{id, comp_code}, ...]}
  */
 router.get("/", async function (req, res, next) {
-  console.log("req.params from invoices", req.params);
   const results = await db.query(
     `SELECT id, comp_code
          FROM invoices`
@@ -25,6 +24,7 @@ router.get("/", async function (req, res, next) {
   const invoices = results.rows;
   return res.json({ invoices: invoices });
 });
+
 
 /** GET /invoices/[id]: returns obj on given invoice with associated company info
  * in the query string: {invoice: {id, amt, paid, add_date, paid_date, company:
@@ -84,9 +84,66 @@ router.post("/", async function (req, res, next) {
            RETURNING id, comp_code, amt, paid, add_date, paid_date`,
     [comp_code, amt]
   );
-
   const invoice = result.rows[0];
+
   return res.status(201).json({ invoice: invoice });
 });
+
+
+/** PUT /invoices/[id]: accepts JSON body, updates an invoice, returns
+ * object of added invoice
+ *
+ * Takes as input JSON: {amt}
+   Returns invoice object {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+
+   If no input JSON, throws BadRequestError. If invoice can't be found, throws
+   NotFoundError.
+*/
+router.put("/:id", async function (req, res, next) {
+  if (!req.body) throw new BadRequestError();
+
+  const { amt } = req.body;
+  const id = req.params.id;
+
+  const result = await db.query(
+    `UPDATE invoices
+           SET amt=$1
+           WHERE id = $2
+           RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+    [amt, id],
+  );
+
+  if (!result.rows[0]) {
+    throw new NotFoundError();
+  }
+
+  const invoice = result.rows[0];
+
+  return res.json({ invoice: invoice });
+});
+
+
+/** DELETE /invoices/[id].
+ *
+ * Deletes invoice with the given id, and returns {status: "deleted"}
+ * if successful. If invoice not found, throws NotFoundError.
+*/
+router.delete("/:id", async function (req, res, next) {
+  const id = req.params.id;
+
+  const result = await db.query(
+    `DELETE FROM invoices WHERE id = $1
+      RETURNING id`,
+    [id],
+  );
+
+  if (!result.rows[0]) {
+    throw new NotFoundError();
+  }
+
+  return res.json({ status: "deleted" });
+});
+
+
 
 module.exports = router;
